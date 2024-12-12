f76903bf2ecc45a5d2d342496c4e5efa0b4da0e4
Ashik512 <ashikiqbal.cse@gmail.com>
2024-12-12 14:10:35 +0600

    comment response entity design done

--- src/main/java/com/example/blog/features/comment/payload/response/CommentResponseDto.java
+++ src/main/java/com/example/blog/features/comment/payload/response/CommentResponseDto.java
@@ -1,4 +1,20 @@
 package com.example.blog.features.comment.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentResponseDto {
+    private Long userId;
+    private Long userName;
+    private String comment;
+    private List<CommentResponseDto> replies = new ArrayList<>();
 }
--- src/main/java/com/example/blog/features/comment/payload/response/PostWithCommentsResponseDto.java
+++ src/main/java/com/example/blog/features/comment/payload/response/PostWithCommentsResponseDto.java
@@ -1,5 +1,18 @@
 package com.example.blog.features.comment.payload.response;
 
-public class PostWithCommentsResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostWithCommentsResponseDto {
+    private Long postId;
+    private String title;
+    private List<CommentResponseDto> comments;
 }
